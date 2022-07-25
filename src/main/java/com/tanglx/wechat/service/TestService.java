9181c0940711f66a863536c7c06a22c9d973df5b
tanglingxiao <tanglingxiao@shkingyu.com>
2022-07-25 17:51:42 +0800

    11

---
+++ src/main/java/com/tanglx/wechat/service/TestService.java
@@ -0,0 +1,16 @@
+package com.tanglx.wechat.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @Describe
+ * @Author tanglingxiao
+ * @Date 2022-07-25
+ */
+@Service
+public class TestService {
+
+    public void aa() {
+        System.out.println("111");
+    }
+}
